a91d32a27525a30a110612a503b142a62464cbbb
will289 <will289@hotmail.com>
2019-03-12 10:41:28 -0400

    renamed package com.piedpiper to com.piedpiper.gui

--- src/com/piedpiper/gui/PiedController.java
+++ src/com/piedpiper/gui/PiedController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.piedpiper;
+package com.piedpiper.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
