fdef2b127e273c5342f0e62b32829b4d95626d4e
ktsuench <will289@hotmail.com>
2019-03-29 03:13:53 -0400

    updated client connection to handle sending data
    updated connection to check if anything in queue before writing to channel

--- src/com/piedpiper/communication/ClientConnectionManager.java
+++ src/com/piedpiper/communication/ClientConnectionManager.java
@@ -26,7 +26,13 @@ package com.piedpiper.communication;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -35,22 +41,83 @@ import java.util.logging.Logger;
  */
 public class ClientConnectionManager {
   private static final Logger LOG = Logger.getLogger(ClientConnectionManager.class.getName());
-  private final SocketChannel channel;
-  private final String name;
+  private final SocketChannel CHANNEL;
+  private final ArrayList<Datagram> DATAGRAMS;
+  private final String NAME;
+  private final Selector SELECTOR;
+  private final TaskHandler TASK_HANDLER;
 
-  public ClientConnectionManager(String name) throws IOException {
-    Datagram updateName = new Datagram(Datagram.DATA_TYPE.UPDATE_ID, name, ServerConnectionManager.SERVER_NAME, name);
+  /**
+   *
+   * @param name should be less than 40 characters
+   * <p>
+   * @throws IOException
+   * @throws com.piedpiper.communication.ChannelSelectorCannotStartException
+   */
+  public ClientConnectionManager(String name) throws IOException, ChannelSelectorCannotStartException {
+    this.TASK_HANDLER = new TaskHandler();
+    this.DATAGRAMS = new ArrayList<>();
+
+    try {
+      SELECTOR = Selector.open();
+    } catch (IOException ex) {
+      LOG.log(Level.SEVERE, null, ex);
+
+      throw new ChannelSelectorCannotStartException("Failed to start up conneciton.");
+    }
+
+    this.NAME = name;
+    this.CHANNEL = SocketChannel.open();
+
+    Runnable selectorTask = () -> {
+      // TODO need to check how to kill thread if required to force quit
+      while (true)
+        try {
+          int readyChannels = SELECTOR.selectNow();
+
+          if (readyChannels == 0)
+            continue;
+
+          Set<SelectionKey> selectedKeys = SELECTOR.selectedKeys();
+          Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+          while (keyIterator.hasNext()) {
+            SelectionKey key = keyIterator.next();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            if (key.isReadable()) {
+
+            } else if (key.isWritable() && this.DATAGRAMS.size() > 0) {
+              buffer.put(this.DATAGRAMS.remove(0).getBytes());
+              buffer.flip();
+              this.CHANNEL.write(buffer);
+            }
+
+            keyIterator.remove();
+          }
+        } catch (IOException ex) {
+          // TODO log this to log file
+          LOG.log(Level.SEVERE, null, ex);
+        }
+    };
+
+    this.TASK_HANDLER.startTask(selectorTask);
+
+    this.DATAGRAMS.add(new Datagram(Datagram.DATA_TYPE.UPDATE_ID, name, ServerConnectionManager.SERVER_NAME, name));
+
+    this.CHANNEL.connect(new InetSocketAddress("localhost", 6000));
+    this.CHANNEL.register(this.SELECTOR, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+  }
+
+  /**
+   *
+   * @param recipient should be less than 40 characters
+   * @param message   should be less than 120 characters
+   */
+  public void sendMessage(String recipient, String message) {
     ByteBuffer buffer = ByteBuffer.allocate(1024);
+    Datagram datagram = new Datagram(Datagram.DATA_TYPE.MESSAGE, NAME, recipient, message);
 
-    this.name = name;
-    this.channel = SocketChannel.open();
-
-    // place datagram in buffer
-    buffer.put(updateName.getBytes());
-    // flip buffer for writing to server
-    buffer.flip();
-
-    this.channel.connect(new InetSocketAddress("localhost", 6000));
-    this.channel.write(buffer);
+    this.DATAGRAMS.add(datagram);
   }
 }
